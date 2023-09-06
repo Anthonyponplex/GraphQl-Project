fad602e7e03bdec928f105259bda092ae3f80731
anthonyponplex <mayakianthony@yahoo.com>
2023-09-06 16:50:20 +0100

    setup Graphql server

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const connectDB = require("./config/db");
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+// connect to database
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(port, console.log(`Server running on port ${port}`));
